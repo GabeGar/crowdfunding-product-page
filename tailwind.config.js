4a404c4ed725d6b0174bb0183ead32da3c70904a
Gabegar <gagarcia190@gmail.com>
2023-09-23 15:40:10 -0400

    init project commit/setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-moderate-cyan':
+                    'hsl(var(--moderate-cyan) / <alpha-value>)',
+                'primary-dark-cyan': 'hsl(var(--dark-cyan) / <alpha-value>)',
+                'neutral-black': 'hsl(var(--black) / <alpha-value>)',
+                'neutral-dark-gray': 'hsl(var(--dark-gray) / <alpha-value>)',
+            },
+            backgroundImage: {
+                'bg-hero-desktop':
+                    "url('/src/assets/images/image-hero-desktop.jpg')",
+                'bg-hero-mobile':
+                    "url('/src/assets/images/image-hero-mobile.jpg')",
+            },
+        },
+    },
+    plugins: [],
+};
