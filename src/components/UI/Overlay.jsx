92da33dd59a0fd10355aa91fb6c171f2ff00f56a
Gabegar <gagarcia190@gmail.com>
2023-09-29 14:02:04 -0400

    updated bg color style to be more inline w/ design

--- src/components/UI/Overlay.jsx
+++ src/components/UI/Overlay.jsx
@@ -1,6 +1,6 @@
 const Overlay = ({ children }) => {
     return (
-        <div className="absolute min-h-screen w-full mx-auto z-10 px-6 py-8 bg-neutral-dark-gray/30">
+        <div className="absolute min-h-screen w-full mx-auto z-10 px-6 py-8 bg-neutral-black/50">
             {children}
         </div>
     );
