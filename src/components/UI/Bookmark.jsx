63e6b420475f327f3830baa93d1518c00f58b25d
Gabegar <gagarcia190@gmail.com>
2023-09-29 15:51:06 -0400

    creates bookmark functionality for mobile

--- src/components/UI/Bookmark.jsx
+++ src/components/UI/Bookmark.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 const Bookmark = () => {
+    const [isBookMarked, setIsBookMarked] = useState(false);
+
+    const circleFill = isBookMarked ? `#3cb4ac` : `#2F2F2F`;
+    const markFill = isBookMarked ? `#fff` : `#B1B1B1`;
+
     return (
-        <button>
+        <button onClick={() => setIsBookMarked((marked) => !marked)}>
             <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
-                    <circle fill="#2F2F2F" cx="28" cy="28" r="28" />
-                    <path fill="#B1B1B1" d="M23 19v18l5-5.058L33 37V19z" />
+                    <circle fill={circleFill} cx="28" cy="28" r="28" />
+                    <path fill={markFill} d="M23 19v18l5-5.058L33 37V19z" />
                 </g>
             </svg>
         </button>
