39f670d8a209aef34525ad77e7d0bb093a68d4f2
Gabegar <gagarcia190@gmail.com>
2023-10-10 19:40:35 -0400

    creates base ts config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,23 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:react-hooks/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    settings: { react: { version: '18.2' } },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+    },
     plugins: ['react-refresh'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
         ],
-        'react/prop-types': 'off',
-        'no-console': 'warn',
-        'no-unused-vars': 'error',
     },
 };
